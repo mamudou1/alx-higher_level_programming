dab7b5607b03030fe7157ea2500e6039af3dc333
mamudou1 <mamudoubojang17@gmail.com>
2022-06-07 12:11:36 +0000

    this is the it

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    tam = len(matrix[0])
-    if tam is 0:
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
         print()
-    else:
-        tam = len(matrix[0])
-        cont = 0
-        for int in matrix:
-            for int2 in int:
-                cont += 1
-                if cont == tam:
-                    print("{:d}".format(int2), end="\n")
-                else:
-                    print("{:d} ".format(int2), end="")
-            cont = 0
