c6bd7c3871460156f84169dae68a43963b1f315c
mamudou1 <mamudoubojang17@gmail.com>
2022-09-06 22:44:08 +0000

    M

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-exports.dict = {
-    89: 1,
-    90: 2,
-    91: 1,
-    92: 3,
-    93: 1,
-    94: 2
-  };
+const list = require('./100-data').list;
+const mapped = list.map(function (value, index) { return value * index; });
+console.log(list);
+console.log(mapped);
